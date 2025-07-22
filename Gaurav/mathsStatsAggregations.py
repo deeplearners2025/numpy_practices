d1a7d4d360111724a89513c2c3067a11cff82631
GGG <deeplearners2025@gmail.com>
2025-07-22 12:03:18 +0530

    Numpy Practice - Broadcasting

--- Gaurav/mathsStatsAggregations.py
+++ Gaurav/mathsStatsAggregations.py
@@ -14,5 +14,11 @@ print(a.mean())
 print(np.mean(a))  #note that mean works both ways
 print(np.median(a))
 print(np.std(a))
+print(10*"+-*-+","\r\n")
 
 #Broadcasting
+a = np.array([[1],[2],[3]])
+print("a.ndim: ",a.ndim)
+b = np.array([10,20,30])
+print("b.ndim: ",b.ndim)
+print(a + b)
