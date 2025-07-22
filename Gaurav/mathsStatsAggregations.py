daab3cbb7f492009e765b1ec228c7911f949f19a
GGG <deeplearners2025@gmail.com>
2025-07-22 11:51:47 +0530

    Numpy Practice - Mean, Median, Std Dev

--- Gaurav/mathsStatsAggregations.py
+++ Gaurav/mathsStatsAggregations.py
@@ -1,8 +1,18 @@
 import numpy as np
 
+# Array Aggregations
 matrix = np.array([[1,2], [3,4]])
 print(matrix)
 print(np.sum(matrix))
 print(np.sum(matrix,axis=0)) #sum across Y-axis (vertical)
 print(np.sum(matrix,axis=1)) #sum across X-axis (horizontal)
 print(10*"+-*-+","\r\n")
+
+# Mean, Median, Std Dev
+a = np.array([1,2,4,5,6])
+print(a.mean())
+print(np.mean(a))  #note that mean works both ways
+print(np.median(a))
+print(np.std(a))
+
+#Broadcasting
