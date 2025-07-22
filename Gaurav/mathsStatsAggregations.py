7fb67cc08b234177e43f8142fa41e96b28577602
GGG <deeplearners2025@gmail.com>
2025-07-22 12:29:03 +0530

    Numpy Practice - Matrix Multiplication

--- Gaurav/mathsStatsAggregations.py
+++ Gaurav/mathsStatsAggregations.py
@@ -18,7 +18,28 @@ print(10*"+-*-+","\r\n")
 
 #Broadcasting
 a = np.array([[1],[2],[3]])
-print("a.ndim: ",a.ndim)
+print("a.shape: ",a.shape)
 b = np.array([10,20,30])
-print("b.ndim: ",b.ndim)
+print("b.shape: ",b.shape)
 print(a + b)
+print(10*"+-*-+","\r\n")
+
+# Matrix Multiplication
+# Remember that the multiplication of the shape (n1,m1) and (n2,m2) will result in the shape (n1,m2), given m1 == n2
+a1 = np.array([[1,2],[3,4]])
+print("a1.shape: ",a1.shape)
+a2 = np.array([[2,0],[5,7]])
+print("a2.shape: ",a2.shape)
+print(np.matmul(a1,a2))
+
+a3 = np.array([[1,2],[3,4]])
+print("a3.shape: ",a3.shape)
+a4 = np.array([[2],[0]])
+print("a4.shape: ",a4.shape)
+print(np.matmul(a3,a4))
+
+A = np.random.rand(3, 4)
+print(A)
+B = np.random.rand(2, 5)
+print(B)
+np.matmul(A, B)  # This will raise an error because m1 != n2, i.e., 4 != 2
